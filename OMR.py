60a5b1b7a5cea717939c5bb22e17196cd1430ef0
Vishwesh Ramanathan <Vishwesh Ramanathan>
2017-03-05 13:13:19 +0530

    OMR code updated, can now detect answer response

--- OMR.py
+++ OMR.py
@@ -9,7 +9,7 @@ import cv2
 import numpy as np
 import math
 
-img_rgb = cv2.imread('Scan10010.jpg')
+img_rgb = cv2.imread('12.jpg')
 
 
 #for bringing down the size
@@ -22,7 +22,7 @@ img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
 
 template = cv2.imread('template1.jpg',0)
 w, h = template.shape[::-1]
-cv2.imshow('original',img_rgb)
+#cv2.imshow('original',img_rgb)
 #for mathing the squares
 res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
 threshold = 0.9
@@ -51,7 +51,7 @@ for j in range (10-z,9):
 shapex=np.sort(shapex)
 shapey=np.sort(shapey)
 
-#can be done better
+
 
 if i==4 :
     pts1 = np.float32([[shapex[6]+h,shapey[7]+w],[shapex[8]+h,shapey[6]+w],[shapex[7]+h,shapey[9]+w],[shapex[9]+h,shapey[8]+w]])
@@ -86,7 +86,7 @@ bub1y=np.zeros((1500),dtype=np.int32)
 loc = np.where( res >= threshold)
 
 
-print loc
+
 i=0
 
 for pt in zip(*loc[::-1]):
@@ -107,7 +107,7 @@ for pt in zip(*loc[::-1]):
                  flag=0
     j=0
     if flag!=0: 
-        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (255,255,0), 0)
+        #cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (255,255,0), 0)
         bub1x[k]=pt[0]
         bub1y[k]=pt[1]
         k=k+1        
@@ -122,14 +122,103 @@ for pt in zip(*loc[::-1]):
 sepx=np.zeros(k,dtype=np.int32)
 sepy=np.zeros(k,dtype=np.int32)
 
-#tempx=bub1x
-tempx=np.sort(bub1x)
-print tempx
-for i in range(0,k):
-    sepx[i]=abs(tempx[i]-tempx[i+1])
+for i in range(0,k-1): 
     sepy[i]=abs(bub1y[i]-bub1y[i+1])
-    print sepx[i],sepy[i]
+    
 
 
+tempy=np.sort(sepy)
+for z in range (0,k-1):
+    if tempy[k-1]==sepy[z]:
+            break
+        
+  
+#for answer evaluation
+omrsep=np.sort(sepy[z+1:k])  
+quessep=omrsep[k-z-2]
+#tempx=bub1x
+#a= np.where(tempx==min(tempx[z+1:k]))
+#tempx[a]=1000
+
+#print min(tempx[z+1:k])
+#print np.where(tempx==min(tempx[z+1:k]))
+#2w for 1 , 3w for 2 .....5w for 4 , 8w for next block 1st
+for y in range (z+1,k):
+    
+    if bub1x[y]>=min(bub1x[z+1:]) and bub1x[y]<min(bub1x[z+1:])+5*w:
+           for n in range (1,21):   
+               if bub1y[y]+h/2> bub1y[z+1]+(n-1)*h and bub1y[y]+h/2< bub1y[z+1]+(n)*h:
+                   print "q" + str(n)
+           
+           cv2.rectangle(img_rgb, (bub1x[y],bub1y[y]), (bub1x[y]+ w,bub1y[y] + h), (255,255,0), 0)
+           if bub1x[y]>=min(bub1x[z+1:]) and bub1x[y]<min(bub1x[z+1:])+2*w:        
+                print "A"
+           if bub1x[y]>=min(bub1x[z+1:])+2*w and bub1x[y]<min(bub1x[z+1:])+3*w:    
+                print "B" 
+           if bub1x[y]>=min(bub1x[z+1:])+3*w and bub1x[y]<min(bub1x[z+1:])+4*w:    
+                print "C"     
+           if bub1x[y]>=min(bub1x[z+1:])+4*w and bub1x[y]<min(bub1x[z+1:])+5*w:    
+                print "D"        
+for y in range (z+1,k):        
+    if bub1x[y]>=min(bub1x[z+1:])+7*w and bub1x[y]<min(bub1x[z+1:])+11*w:
+           for n in range (1,21):   
+               if bub1y[y]+h/2> bub1y[z+1]+(n-1)*h and bub1y[y]+h/2< bub1y[z+1]+(n)*h:
+                   print "q" + str(n+20)
+           
+           cv2.rectangle(img_rgb, (bub1x[y],bub1y[y]), (bub1x[y]+ w,bub1y[y] + h), (255,255,0), 0)
+           if bub1x[y]>=min(bub1x[z+1:])+7*w and bub1x[y]<min(bub1x[z+1:])+8*w:        
+                print "A"
+           if bub1x[y]>=min(bub1x[z+1:])+8*w and bub1x[y]<min(bub1x[z+1:])+9*w:    
+                print "B" 
+           if bub1x[y]>=min(bub1x[z+1:])+9*w and bub1x[y]<min(bub1x[z+1:])+10*w:    
+                print "C"     
+           if bub1x[y]>=min(bub1x[z+1:])+10*w and bub1x[y]<min(bub1x[z+1:])+11*w:    
+                print "D" 
+for y in range (z+1,k):    
+    if bub1x[y]>=min(bub1x[z+1:])+14*w and bub1x[y]<min(bub1x[z+1:])+18*w:
+           for n in range (1,21):   
+               if bub1y[y]+h/2> bub1y[z+1]+(n-1)*h and bub1y[y]+h/2< bub1y[z+1]+(n)*h:
+                   print "q" + str(n+40)
+           
+           cv2.rectangle(img_rgb, (bub1x[y],bub1y[y]), (bub1x[y]+ w,bub1y[y] + h), (255,255,0), 0)
+           if bub1x[y]>=min(bub1x[z+1:])+14*w and bub1x[y]<min(bub1x[z+1:])+15*w:        
+                print "A"
+           if bub1x[y]>=min(bub1x[z+1:])+15*w and bub1x[y]<min(bub1x[z+1:])+16*w:    
+                print "B" 
+           if bub1x[y]>=min(bub1x[z+1:])+16*w and bub1x[y]<min(bub1x[z+1:])+17*w:    
+                print "C"     
+           if bub1x[y]>=min(bub1x[z+1:])+17*w and bub1x[y]<min(bub1x[z+1:])+18*w:    
+                print "D"   
+for y in range (z+1,k):    
+    if bub1x[y]>=min(bub1x[z+1:])+20*w and bub1x[y]<min(bub1x[z+1:])+24*w:
+           for n in range (1,21):   
+               if bub1y[y]+h/2> bub1y[z+1]+(n-1)*h and bub1y[y]+h/2< bub1y[z+1]+(n)*h:
+                   print "q" + str(n+60)
+           
+           cv2.rectangle(img_rgb, (bub1x[y],bub1y[y]), (bub1x[y]+ w,bub1y[y] + h), (255,255,0), 0)
+           if bub1x[y]>=min(bub1x[z+1:])+20*w and bub1x[y]<min(bub1x[z+1:])+21*w:        
+                print "A"
+           if bub1x[y]>=min(bub1x[z+1:])+21*w and bub1x[y]<min(bub1x[z+1:])+22*w:    
+                print "B" 
+           if bub1x[y]>=min(bub1x[z+1:])+22*w and bub1x[y]<min(bub1x[z+1:])+23*w:    
+                print "C"     
+           if bub1x[y]>=min(bub1x[z+1:])+23*w and bub1x[y]<min(bub1x[z+1:])+24*w:    
+                print "D"
+for y in range (z+1,k):
+    if bub1x[y]>=min(bub1x[z+1:])+27*w and bub1x[y]<min(bub1x[z+1:])+31*w:
+           for n in range (1,21):   
+               if bub1y[y]+h/2> bub1y[z+1]+(n-1)*h and bub1y[y]+h/2< bub1y[z+1]+(n)*h:
+                   print "q" + str(n+80)
+           
+           cv2.rectangle(img_rgb, (bub1x[y],bub1y[y]), (bub1x[y]+ w,bub1y[y] + h), (255,255,0), 0)
+           if bub1x[y]>=min(bub1x[z+1:])+27*w and bub1x[y]<min(bub1x[z+1:])+28*w:        
+                print "A"
+           if bub1x[y]>=min(bub1x[z+1:])+28*w and bub1x[y]<min(bub1x[z+1:])+29*w:    
+                print "B" 
+           if bub1x[y]>=min(bub1x[z+1:])+29*w and bub1x[y]<min(bub1x[z+1:])+30*w:    
+                print "C"     
+           if bub1x[y]>=min(bub1x[z+1:])+30*w and bub1x[y]<min(bub1x[z+1:])+31*w:    
+                print "D"               
+     
 cv2.imshow('bubbledetected',img_rgb)
 cv2.waitKey(0)
